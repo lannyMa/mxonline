98b08fb14e51cd3b46b7139650141ee11ca65c0c
maotai <iher@foxmail.com>
2018-02-07 21:47:16 +0800

    实现根据城市/课程机构筛选

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -19,7 +19,7 @@ class CityDict(models.Model):
 class CourseOrg(models.Model):
     name = models.CharField(max_length=20, verbose_name="课程机构")
     desc = models.TextField(verbose_name="课程机构描述")
-    catagory = models.CharField(max_length=20, default="pxjg", choices=(("pxjg", "培训机构"), ("gr", "个人"), ("gx", "高校")),
+    category = models.CharField(max_length=20, default="pxjg", choices=(("pxjg", "培训机构"), ("gr", "个人"), ("gx", "高校")),
                                 verbose_name="机构类别")
     click_nums = models.IntegerField(default=0, verbose_name="课程机构点击数")
     fav_nums = models.IntegerField(default=0, verbose_name="收藏人数")
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -3,13 +3,41 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic.base import View
 from organization.models import CourseOrg, CityDict
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
 
 class OrgView(View):  # 课程机构列表页
     def get(self, request):
-        all_orgs = CourseOrg.objects.all()
-        all_citys = CityDict.objects.all()
+        all_orgs = CourseOrg.objects.all()  # 所有课程机构
+
+        all_citys = CityDict.objects.all()  # 所有城市列表
+
+        # 取出筛选城市
+        city_id = request.GET.get("city", "")
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 取出筛选培训机构
+        category = request.GET.get('ct', "")
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        org_nums = all_orgs.count()  # 多少家课程机构
+        # 分页模块
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        # all_orgs = ['john', 'edward', 'josh', 'frank']
+
+        # Provide Paginator with the request object for complete querystring generation
+        p = Paginator(all_orgs, 3, request=request)
+        orgs = p.page(page)
+
         return render(request, 'org-list.html', {
-            "all_orgs": all_orgs,
-            "all_citys": all_citys
+            "all_orgs": orgs,
+            "all_citys": all_citys,
+            'org_count': org_nums,
+            'city_id': city_id,
+            'category': category,
         })
